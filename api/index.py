f30d71252213d71117f64c9ff376e6de3018029c
reprogamaco <76619967+reprogamaco@users.noreply.github.com>
2023-05-25 21:38:37 +0300

    made css good a litle

--- api/index.py
+++ api/index.py
@@ -11,10 +11,11 @@ def home():
        <meta name="viewport" content="width=device-width,initial-scale=1"/>
   </head>
   <body>
-    <h1 style="center;font-size: 20px; font-weight:500"> Homepage </h1>
+    <h1 style="text-align: center;font-size: 30px; font-weight:500"> Homepage </h1>
     <div style="padding-left: 12px;padding-right:12px;background-color:whitesmoke;text-align: center; padding-top: 10px; border-radius: 10px;">
-         <button style="padding: 12px 20px; border-radius: 8px; float: center; padding-top: 20px; color: blue;"> Login </button>
-         <button style="padding: 12px 20px; border-radius: 8px; float: center; padding-top: 5px; color: lightblue;"> Login </button>
+         <button style="padding: 12px 20px; border-radius: 8px; float: center; background-color: blue;color: white"> Login </button>
+         <br/>
+         <button style="padding: 12px 20px; border-radius: 8px; float: center; background-color: lightblue;color:white"> Login </button>
     </div>
   </body>
   </html>
