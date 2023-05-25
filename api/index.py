2acf3507347f268991e15a190432a46080c38bec
reprogamaco <76619967+reprogamaco@users.noreply.github.com>
2023-05-25 21:39:44 +0300

    green button login

--- api/index.py
+++ api/index.py
@@ -13,7 +13,7 @@ def home():
   <body>
     <h1 style="text-align: center;font-size: 30px; font-weight:500"> Homepage </h1>
     <div style="padding-left: 12px;padding-right:12px;background-color:whitesmoke;text-align: center; padding-top: 10px; border-radius: 10px;">
-         <button style="padding: 12px 20px; border-radius: 8px; float: center; background-color: blue;color: white"> Login </button>
+         <button style="padding: 12px 20px; border-radius: 8px; float: center; background-color: green;color: white"> Login </button>
          <br/>
          <button style="padding: 12px 20px; border-radius: 8px; float: center; background-color: lightblue;color:white"> Login </button>
     </div>
