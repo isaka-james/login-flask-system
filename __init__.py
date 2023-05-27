da4f4edd18e2775116170333befbfa64aeea9b8d
reprogamaco <76619967+reprogamaco@users.noreply.github.com>
2023-05-27 14:48:15 +0300

    test3

---
+++ __init__.py
@@ -0,0 +1,1 @@
+# For Pythonic packages 
--- app.py
+++ app.py
@@ -1,7 +1,69 @@
-from flask import Flask
+from flask import Flask,session,url_for,render_template,redirect
+import random
+import string
 
-app = Flask('__name__')
+app=Flask(__name__)
+
+# getting the secret key for the sessions
+letters = string.ascii_lowercase
+app.secret_key = ''.join(random.choice(letters) for i in range(32)) # 32 random characters
+
+
 
 @app.route('/')
-def index():
-  return 'hello masterplan my name is drake'
+def dash(): 
+  if 'username' in session:
+    return redirect(f'/dash/{session["username"]}')
+  else:
+    return render_template('login.html',title='Login to Your Account')
+
+
+@app.route('/login')
+def login():
+  if 'username' in session:
+    return redirect(f'/dash/{session["username"]}')
+  else:
+    return render_template('login.html',title='Login to Your Account')
+    
+@app.route('dash/<name>')
+def home():
+  if not 'username' in session:
+    return redirect('/login')
+  title1 = 'Home Page'
+  session_usr = session['username']
+  return render_template('/dashboard.html',title=title1,name = session_usr)
+
+@app.route('/action',methods=['GET','POST'])
+def action():
+  if 'username' in session:
+    return redirect(f'/dash/{session["username"]}')
+
+  # we deal with the post request
+  if request.method == 'POST':
+    try: # for security issue 
+      username = request.form['username']
+      password = request.form['password']
+    except:
+      return render_template('login.html',message='Invalid Request',title='Try login again!')
+  else: # when the user send the get request
+    return render_template('login.html',message='Invalid Request',title='Try login again!')
+
+  # implement logarithm here to check the real password from the server(database)
+  real_pwd = 'master123'
+  real_username = 'masterplan@gmai.com'
+
+  if password == real_pwd:
+    # hoorey
+    session['username'] = username
+    return redirect(f'/dash/{session["username"]}')
+  else:
+    return render_template('login.html',message="Wrong password or Username",title='Login Failed Try agian!')
+
+@app.route('/Logout')
+def logout():
+  if 'username' in session:
+    session.pop('username')
+  return redirect('/login')
+
+if __name__=='__main__':
+  app.run(debug=True)
